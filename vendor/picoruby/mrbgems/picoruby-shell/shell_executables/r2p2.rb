d44ca826cb4fa082b0a0517f7776a99b61739747
ryosk7 <uchiryo7@gmail.com>
2026-04-08 06:50:50 +0900

    Record vendor runtime fixes

---
+++ vendor/picoruby/mrbgems/picoruby-shell/shell_executables/r2p2.rb
@@ -0,0 +1,25 @@
+if ENV['WIFI_MODULE'] == "cyw43"
+  if Shell.get_device(:gpio, 'TRIGGER_NMBLE')&.low?
+    load "/bin/nmble"
+  end
+  ARGV[0] = "--check-auto-connect"
+  load "/bin/wifi_connect"
+  ARGV.clear
+end
+
+if File.exist?("#{ENV['HOME']}/app.mrb")
+  puts "Loading app.mrb"
+  load "#{ENV['HOME']}/app.mrb"
+elsif File.exist?("#{ENV['HOME']}/app.rb")
+  puts "Loading app.rb"
+  load "#{ENV['HOME']}/app.rb"
+else
+  require 'dfu'
+  app = DFU::BootManager.resolve
+  if app
+    puts "Loading #{app}"
+    load app
+  else
+    puts "No app found"
+  end
+end
